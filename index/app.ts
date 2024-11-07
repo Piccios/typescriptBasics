b6d16f8155de25b49333320b2d42163d91812d04
Piccio <lorenzo.hl.picchi@gmail.com>
2024-11-07 16:07:22 +0100

    set index folder

---
+++ index/app.ts
@@ -0,0 +1,15 @@
+
+let test:number;
+test = 5;
+
+// TS viene compilato: per testare le funzionalita' dovro' avviare il compilatore e tradurre il codice in JS
+
+// TS e' la base di Angular e Nestjs(back-end)
+
+
+
+
+
+
+
+
