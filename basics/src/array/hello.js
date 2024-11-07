8d4acf129b62108926b93ed4c79a5c6117946dce
Piccio <lorenzo.hl.picchi@gmail.com>
2024-11-07 13:49:33 +0100

    restarted project

--- basics/src/array/hello.js
+++
@@ -1,4 +0,0 @@
-var name = "Lorenzo";
-var age = 28;
-var text = "Ciao il mio nome e' " + name + " e ho " + age + " anni.";
-console.log(text);
--- basics/src/array/hello.ts
+++
@@ -1,6 +0,0 @@
-let nome: string = "Lorenzo";
-let age: number = 28;
-
-let text: string = "Ciao il mio nome e' " + nome + " e ho " + age + " anni.";
-
-console.log(text);
