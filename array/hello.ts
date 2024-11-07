b61ed1a5c26d8ed7f8ac0a32b7d76e2c641d3e3a
Piccio <lorenzo.hl.picchi@gmail.com>
2024-11-07 13:09:33 +0100

    redeclared a variable name

--- array/hello.ts
+++ array/hello.ts
@@ -1,6 +1,6 @@
-let name: string = "Lorenzo";
+let nome: string = "Lorenzo";
 let age: number = 28;
 
-let text: string = "Ciao il mio nome e' " + name + " e ho " + age + " anni.";
+let text: string = "Ciao il mio nome e' " + nome + " e ho " + age + " anni.";
 
 console.log(text);
