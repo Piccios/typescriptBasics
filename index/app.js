5ee8501fb144a4d33dc040ae2fffc2d913ce8fee
Piccio <lorenzo.hl.picchi@gmail.com>
2024-11-07 16:19:17 +0100

    first function on ts is working

---
+++ index/app.js
@@ -0,0 +1,7 @@
+// TS viene compilato: per testare le funzionalita' dovro' avviare il compilatore e tradurre il codice in JS
+// TS e' la base di Angular e Nestjs(back-end)
+// FUNZIONI
+function somma(a, b) {
+    return a + b;
+}
+console.log(somma(5, 5));
--- index/app.ts
+++ index/app.ts
@@ -1,12 +1,15 @@
 
-let test:number;
-test = 5;
 
 // TS viene compilato: per testare le funzionalita' dovro' avviare il compilatore e tradurre il codice in JS
 
 // TS e' la base di Angular e Nestjs(back-end)
 
+// FUNZIONI
 
+function somma(a: number,b: number) {
+    return a+b
+}
+console.log(somma(5,5))
 
 
 
